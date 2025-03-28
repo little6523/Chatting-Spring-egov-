b09bc8f66ea68ebacff98cc646df997933f0606f
LeeHyeonjun <little6523@naver.com>
2025-03-28 23:41:44 +0900

    전자정부프레임워크 로컬환경에서 채팅 테스트 완료

--- src/main/webapp/js/client.js
+++ src/main/webapp/js/client.js
@@ -1,64 +1,63 @@
 const USER = {
-    name: "user" + Math.random()
+	name: "user" + Math.random()
 }
 
-function onopen() {
-    console.log("WebSocket 연결 성공");
-}
+SOCKET.connect();
 
-function onmessage(event) {
-    const json = JSON.parse(event.data);
+SOCKET.init(
+	() => {
+		console.log("WebSocket 연결 성공");
+	},
+	(event) => {
+		const json = JSON.parse(event.data);
+		if (json.hasOwnProperty("userNumber") && json.hasOwnProperty("userNames")) {
+			document.getElementById("userNumber").textContent = json.userNumber;
 
-    if (json.hasOwnProperty("userNumber") && json.hasOwnProperty("userNames")) {
-        document.getElementById("userNumber").textContent = json.userNumber;
+			const participantsList = document.getElementById("participantsList");
+			let users = "";
+			json.userNames.forEach(name => {
+				if (name == USER.name) {
+					users += "<li class='participant'> 나: " + name + "</li>";
+				} else {
+					users += "<li>" + name + "</li>";
+				}
+			});
+			participantsList.innerHTML = users;
+			return;
+		}
 
-        const participantsList = document.getElementById("participantsList");
-        let users = "";
-        json.userNames.forEach(name => {
-            if (name == USER.name) {
-                users += "<li class='participant'> 나: " + name + "</li>";
-            } else {
-                users += "<li>" + name + "</li>";
-            }
-        });
-        participantsList.innerHTML = users;
-        return;
-    }
-
-    if (json.hasOwnProperty("name") && json.hasOwnProperty("message")) {
-        const chatMessages = document.getElementById("chatMessages");
-        let newMessage = "<div class='message received'>" + json.name + ": " + json.message + "</div>";
-        chatMessages.innerHTML += newMessage;
-        return;
-    }
-}
-
-SOCKET.connect(onopen, onmessage);
+		if (json.hasOwnProperty("name") && json.hasOwnProperty("message")) {
+			const chatMessages = document.getElementById("chatMessages");
+			let newMessage = "<div class='message received'>" + json.name + ": " + json.message + "</div>";
+			chatMessages.innerHTML += newMessage;
+			return;
+		}
+	});
 
 function sendMessage() {
-    data = {};
-    data.message = document.getElementById("messageInput").value;  // 입력된 메시지 가져오기
-    SOCKET.socket.send(JSON.stringify(data));  // 메시지 전송
-    document.getElementById("messageInput").value = "";  // 입력창 초기화
-    
-    let selfMessage = "<div class='message sent'>" + '나: ' + data.message + "</div>";
-    let chatMessages = document.getElementById("chatMessages");
-    chatMessages.innerHTML += selfMessage;
+	data = {};
+	data.message = document.getElementById("messageInput").value;  // 입력된 메시지 가져오기
+	SOCKET.socket.send(JSON.stringify(data));  // 메시지 전송
+	document.getElementById("messageInput").value = "";  // 입력창 초기화
+
+	let selfMessage = "<div class='message sent'>" + '나: ' + data.message + "</div>";
+	let chatMessages = document.getElementById("chatMessages");
+	chatMessages.innerHTML += selfMessage;
 }
 
 function connectWebSocket() {
-    const modal = document.getElementById('nicknameModal');
-    const nicknameInput = document.getElementById('nicknameInput');
+	const modal = document.getElementById('nicknameModal');
+	const nicknameInput = document.getElementById('nicknameInput');
 
-    const nickname = nicknameInput.value.trim();
-    if (nickname) {
-        // 닉네임을 저장하고 모달 닫기
-        USER.name = nickname;
-        document.getElementById('username').textContent = nickname;
-        modal.style.display = 'none';
-    } else {
-        alert('닉네임을 입력해주세요!');
-    }
+	const nickname = nicknameInput.value.trim();
+	if (nickname) {
+		// 닉네임을 저장하고 모달 닫기
+		USER.name = nickname;
+		document.getElementById('username').textContent = nickname;
+		modal.style.display = 'none';
+	} else {
+		alert('닉네임을 입력해주세요!');
+	}
 
-    SOCKET.socket.send(JSON.stringify(USER));
+	SOCKET.socket.send(JSON.stringify(USER));
 }
