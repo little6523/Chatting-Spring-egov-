b75c7ceec1b4826cd03b80d52d817576893da5f8
LeeHyeonjun <little6523@naver.com>
2025-04-17 17:04:18 +0900

    마이페이지에서 프로필 이미지와 닉네임 변경 시 같은 채팅방에 참여중이던 유저들에게도 똑같이 적용되게 구현

--- src/main/webapp/js/client.js
+++ src/main/webapp/js/client.js
@@ -6,7 +6,8 @@ const USER = {
 const ROOM_INFO = {
 	roomSeq: sessionStorage.getItem('roomSeq'),
 	roomName: document.getElementById('roomName').innerText,
-	users: {},   // 소켓은 연결되어있지 않으나 채팅방에 참여중인 유저
+	seqToUser: {},   // 소켓은 연결되어있지 않으나 채팅방에 참여중인 유저
+	UserToSeq: {},
 	currentUsers: [],   // 현재 소켓 연결된 유저
 	profileImages: {}
 }
@@ -17,7 +18,8 @@ function initParticipants() {
 		data.roomSeq = sessionStorage.getItem('roomSeq');
 		common.sendAjax('post', '/api/participants', data, function(response, xhr) {
 			response.participants.forEach(participant => {
-				ROOM_INFO.users[participant.seq] = participant.nickname;
+				ROOM_INFO.seqToUser[participant.seq] = participant.nickname;
+				ROOM_INFO.UserToSeq[participant.nickname] = participant.seq;
 			});
 			resolve();
 		})
@@ -35,8 +37,8 @@ function fetchProfileImage(userSeq) {
 }
 
 // 여러 유저의 프로필 이미지를 초기화하는 함수
-function initProfileImage() {
-	const promises = Object.keys(ROOM_INFO.users).map(seq => {
+function initProfileImages() {
+	const promises = Object.keys(ROOM_INFO.seqToUser).map(seq => {
 		return fetchProfileImage(seq).then(image => {
 			if (!ROOM_INFO.profileImages.hasOwnProperty(seq) || ROOM_INFO.profileImages[seq] !== image) {
 				ROOM_INFO.profileImages[seq] = image;
@@ -88,7 +90,7 @@ function makeChatbox(json) {
 		let chat = line.split(':');
 		let profile = "<div class='mini-profile'>";
 		profile += "<img src='data:image/jpg;base64," + ROOM_INFO.profileImages[chat[0]] + "' alt='프로필' class='profile-img'>";
-		profile += "<span class='username' id='username'>" + ROOM_INFO.users[chat[0]] + "</span>";
+		profile += "<span class='username'>" + ROOM_INFO.seqToUser[chat[0]] + "</span>";
 		profile += "</div>"
 
 		if (chat[0] == sessionStorage.getItem('seq')) {
@@ -112,7 +114,7 @@ function makeChatbox(json) {
 $(document).ready(function() {
 	initParticipants()
 		.then(() => {
-			initProfileImage()
+			initProfileImages()
 				.then(() => {
 					imageLoadingDone = true;
 					showBufferedMessages();
@@ -155,7 +157,7 @@ $(document).ready(function() {
 	});
 })
 
-let messageBuffer = [];
+let messageBuffer = []
 let imageLoadingDone = false;
 
 function showBufferedMessages() {
@@ -186,9 +188,26 @@ function connectSocket() {
 				if (json.hasOwnProperty("newUser")) {
 					updateParticipants(json.newUser.name, 'in');
 					initParticipants()
-						.then((newUser) => {
-							initProfileImage();
-						});
+						.then(() => {
+							fetchProfileImage(ROOM_INFO.UserToSeq[json.newUser.name].toString())
+								.then((image) => {
+									ROOM_INFO.profileImages[ROOM_INFO.UserToSeq[json.newUser.name]] = image;
+									const miniProfiles = document.getElementsByClassName('mini-profile');
+									const values = Object.values(ROOM_INFO.seqToUser);
+
+									for (let i = 0; i < miniProfiles.length; i++) {
+										const profile = miniProfiles[i];
+
+										let profileImage = profile.querySelector('.profile-img');
+										let nickname = profile.querySelector('.username');
+
+										if (!values.includes(nickname.textContent)) {
+											nickname.textContent = json.newUser.name;
+											profileImage.src = 'data:image/jpg;base64,' + image;
+										}
+									}
+								})
+						})
 
 					return;
 				}
@@ -234,7 +253,7 @@ function sendMessage() {
 	let message = "<div class='messageBox sent'>"
 	let profile = "<div class='mini-profile'>";
 	profile += "<img src='data:image/jpg;base64," + ROOM_INFO.profileImages[userSeq] + "' alt='프로필' class='profile-img'>";
-	profile += "<span class='username' id='username'>" + ROOM_INFO.users[userSeq] + "</span>";
+	profile += "<span class='username' id='username'>" + ROOM_INFO.seqToUser[userSeq] + "</span>";
 	profile += "</div>"
 	message += profile;
 	let selfMessage = "<div class='message sent'>" + document.getElementById("messageInput").value + "</div>";
